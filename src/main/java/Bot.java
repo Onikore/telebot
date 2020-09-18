38270be9ed41dd0d4951151776c75240734bf702
Dmitry Grishin <grishinda10@ya.ru>
2020-09-19 01:11:59 +0500

    ф

---
+++ src/main/java/Bot.java
@@ -0,0 +1,91 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Bot extends TelegramLongPollingBot {
+
+    private static String BOT_NAME;
+    private static String BOT_TOKEN;
+
+    public static void main(String[] args) {
+        FileInputStream fileInputStream;
+        Properties prop = new Properties();
+        try {
+            fileInputStream = new FileInputStream("src/main/resources/config.properties");
+            prop.load(fileInputStream);
+            BOT_NAME = prop.getProperty("BOT_NAME");
+            BOT_TOKEN = prop.getProperty("BOT_TOKEN");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Метод для приема сообщений.
+     *
+     * @param update Содержит сообщение от пользователя.
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    /**
+     * Метод для настройки сообщения и его отправки.
+     *
+     * @param chatId id чата
+     * @param s      Строка, которую необходимот отправить в качестве сообщения.
+     */
+    public synchronized void sendMsg(String chatId, String s) {
+        Logger logger = Logger.getLogger(Bot.class.getName());
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            logger.log(Level.SEVERE, "Exception: " + e.toString());
+        }
+    }
+
+    /**
+     * Метод возвращает имя бота, указанное при регистрации.
+     *
+     * @return имя бота
+     */
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    /**
+     * Метод возвращает token бота для связи с сервером Telegram
+     *
+     * @return token для бота
+     */
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+}
